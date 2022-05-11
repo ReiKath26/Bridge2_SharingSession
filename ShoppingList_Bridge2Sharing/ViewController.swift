2a2559f190300324f922f9a0ddad29d36b364dfa
Kathleen <kathy.febiola02@gmail.com>
2022-05-11 20:36:16 +0700

    Initial Commit

---
+++ ShoppingList_Bridge2Sharing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList_Bridge2Sharing
+//
+//  Created by Kathleen Febiola Susanto on 11/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
